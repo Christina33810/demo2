8dcc11b3a21d9f3289a8ab8659e9e45ea3f2f545
Christina33810 <luckyaroundme123@gmail.com>
2025-11-18 15:57:51 +0800

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -9,23 +9,24 @@ if "expenses" not in st.session_state:
   st.session_state.expense = pd.Dataframe(columns=['Date', 'Category', 'Amount', 'Description'])
 
 #Create sth
-with st.form["expense_from"):
- st.subheader("Add New Expense")
- date = st.date_input ("Date")
- category = st.selectbox("Category", ["Food", "Transport", "Entertainment", "Bills", "Other"])
- amount = st.number_input( "Amount", min_value=0.0, step=0.01)
- description = st.text_input ("Description")
+with st.form("expense_from"):
+  st.subheader("Add New Expense")
+  date = st.date_input ("Date")
+  category = st.selectbox("Category", ["Food", "Transport", "Entertainment", "Bills", "Other"])
+  amount = st.number_input( "Amount", min_value=0.0, step=0.01)
+  description = st.text_input ("Description")
 
-submitted = st. form_submit_button ("|
-if submitted:
- new_expense = pd.DaraFrame({
-  'Date':[date],
-  'Category': [category],
-  'Amount':[amout],
-  'Description':[Description]
-})
-st.session_state.expenses = pd.contact([st.session_state.expenses, new_expense], ignore_index = True)
-st.success("Expense added successfully!")
+  submitted = st. form_submit_button ("Add Expense")\
+                                      
+  if submitted:
+     new_expense = pd.DaraFrame({
+      'Date':[date],
+      'Category': [category],
+      'Amount':[amout],
+      'Description':[Description]
+    })
+    st.session_state.expenses = pd.contact([st.session_state.expenses, new_expense], ignore_index = True)
+    st.success("Expense added successfully!")
 
 if not st.session_state.expense.empty:
   st.subheader("Your Expense")
